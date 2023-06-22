a98dcad3fc95c7d02b37cfd7f9ec61dc3d48e02f
Parth Prajapati <51105418+parthosc@users.noreply.github.com>
2023-06-22 20:22:48 +0530

    server

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -1,4 +1,4 @@
-import type { NextResponse, NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from "next/server";
 
 export default function handler(req: NextRequest, res: NextResponse) {
     res.status(200).json({ name: 'John Doe' })
