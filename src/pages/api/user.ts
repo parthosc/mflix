169b80c7a05356da53a50179a55765c40f5f9f5a
Parth Prajapati <51105418+parthosc@users.noreply.github.com>
2023-06-22 20:36:18 +0530

    Api

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextRequest, res: NextResponse) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
     res.status(200).json({ name: 'John Doe' })
 }
