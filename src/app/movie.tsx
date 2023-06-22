0b1410e831380283b48052a0ef77f42b586a9488
Parth Prajapati <51105418+parthosc@users.noreply.github.com>
2023-06-22 20:45:24 +0530

    movie

---
+++ src/app/movie.tsx
@@ -0,0 +1,44 @@
+import { useRouter } from "next/router";
+import clientPromise from "../lib/mongodb";
+
+export default function Movies({ movies }: { movies: any }) {
+    const router = useRouter()
+
+    return (
+        <div>
+            <h1>Top 20 Movies of All Time</h1>
+            <p>
+                <small>(According to Metacritic)</small>
+            </p>
+            <ul>
+                {movies.map((movie: any) => (
+                    <li>
+                        <h2>{movie.title}</h2>
+                        <p>{movie.plot}</p>
+                        <button type="button" onClick={() => router.push(`/movies/${movie.title.replaceAll(" ", "-")}`)}>View</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export async function getServerSideProps() {
+    try {
+        const client = await clientPromise;
+        const db = client.db("sample_mflix");
+
+        const movies = await db
+            .collection("movies")
+            .find({})
+            .sort({ metacritic: -1 })
+            .limit(20)
+            .toArray();
+
+        return {
+            props: { movies: JSON.parse(JSON.stringify(movies)) },
+        };
+    } catch (e) {
+        console.error(e);
+    }
+}
