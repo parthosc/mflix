268cc6fdee19c92aaad7974e2779e38f8ef2b830
Akshay Bharadva <akshaybharadva19@gmail.com>
2023-06-22 20:03:32 +0530

    create-next-app

--- next.config.ts
+++
@@ -1,5 +0,0 @@
-const config = {
-
-}
-
-module.export = config;
--- pages/orm.tsx
+++
@@ -1,9 +0,0 @@
-import {PrismaClient} from '@prisma/client'
-
-export default function Home() {
-    // const prisma = new PrismaClient()
-    
-    return <div>
-          {/* {console.log("##prisma",prisma)} */}
-    </div>
-}
